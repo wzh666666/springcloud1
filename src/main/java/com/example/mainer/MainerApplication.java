a8ec9e50b66817cb0fb92a2d2a2e833581ace0a0
stefuns <stefuns@163.com>
2019-05-01 16:21:34 +0800

    Initial commit

---
+++ src/main/java/com/example/mainer/MainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainerApplication.class, args);
+    }
+
+}
