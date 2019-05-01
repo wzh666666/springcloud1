12b1f32b35dd17889fa57aa8b63c677fe845d572
stefuns <stefuns@163.com>
2019-05-01 16:22:31 +0800

    123456

--- src/main/java/com/example/mainer/MainerApplication.java
+++ src/main/java/com/example/mainer/MainerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MainerApplication.class, args);
     }
